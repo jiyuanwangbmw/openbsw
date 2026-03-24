175315a4596fa8e5bf9cbd5032dd23834b28a0c3
jiyuanwangbmw <jiyuanwangbmw@users.noreply.github.com>
2026-03-24 14:08:11 +0000

    deploy: 499333653f770b7952ab547ebf2661715601bad7

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,94 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/dc3/classdocan_1_1DoCanConnection.html#aa5a24792c1e7fcb081086efc94c0e080",
+"d1/d4e/classuds_1_1DiagJobRoot.html",
+"d1/dd4/classuds_1_1DiagReturnCode.html#aedc5ff7985a1ed085f2bc9dcbc722919a879ea7e5629004f6bcd90dae3ccea523",
+"d2/d8c/classio_1_1MemoryQueue_1_1Writer.html#af2612b700df8bf4d03625b6784978e42",
+"d2/df6/classbios_1_1IEcuPowerStateController.html#aed6d2fc4f01d9ae178abb5b2fde42db2",
+"d3/d9d/structuds_1_1InputOutputControlByIdentifier_1_1IOControlParameter.html#a2f3b1684e776ef2eed337c3a28e03538",
+"d4/d2b/classlogger_1_1BufferedLoggerOutput.html",
+"d4/ddc/classuds_1_1MultipleReadDataByIdentifier.html#a775c30fc03eea19e9b476042562bf948",
+"d5/dd0/classdoip_1_1DoIpUdpConnection.html#a2d5937e899c1f7f29c04688ae897d3a1",
+"d6/d57/structutil_1_1estd_1_1internal_1_1__derived__type_3_01D_00_01T_00_01true_01_4.html",
+"d7/d1c/Bit_8h.html#accb3f048da9d12f1a9f80f017d63b9f7",
+"d7/de7/classutil_1_1stream_1_1StringBufferOutputStream.html#a2bee068f30b198c2947d92468136a57e",
+"d8/d81/classmiddleware_1_1queue_1_1QueueBase.html#ae6021b5c7af43a88e6e2519e3c0b8f51",
+"d9/d02/structestd_1_1internal_1_1type__for__size__helper_3_018U_01_4.html#a995cc5024891bc10dddeb49595ce6257",
+"d9/db6/structutil_1_1format_1_1ParamFlags.html",
+"da/d35/forward__list_8h.html#ad23e9cc40af5370637beef19f2c1c82f",
+"da/db8/classuds_1_1ICommunicationStateListener.html#a538e134f6ac395429643f58194103407a652b5a704a890107e74c57b62078d8ec",
+"db/d1f/Tracer_8h_source.html",
+"db/dad/big__endian_8h.html#a28cd54486ea3ba81a4f4d1014c3ff171",
+"dc/d14/classasync_1_1internal_1_1TaskImpl.html#add65ccb4a04c2bb11ac95ecc7c415c16",
+"dc/d7e/classenetphy_1_1Tja1101.html#aff5231b841ef7ba11411a1c932afd8f4aa921d10f8c8932425d23dc6e77e77307",
+"dd/d16/structtimer_1_1Timeout.html#a479f40469490ec7cb0d6a64bce7cf328",
+"dd/d9b/classutil_1_1command_1_1CommandContext.html#a46d793261e84ef5546f24fe5b73b004f",
+"de/d69/classruntime_1_1FunctionRuntimeStatistics.html#abe5d2517ce2512e08b4ff72ed5a8e429",
+"df/d0e/classestd_1_1declare_1_1ordered__map.html#a5b7ea5097d399d562dc6d16254293896",
+"df/d9d/classutil_1_1stream_1_1TaggedOutputHelper.html#a215970385ec60a22fa6482535595f86e",
+"namespaces.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
